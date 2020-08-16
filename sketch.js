dd29bfae3642b21d08a50df69adaf09c16d37cdb
lohit2007 <69612175+lohit2007@users.noreply.github.com>
2020-08-16 12:44:47 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,53 +1,39 @@
-var banana, obstacle, player, back, score;
-
-var bananaImage, obstacleImage, playerImage, backImage;
+var bananaImage, obstacleImage,obstacleGroup,score, backImage, player_running;
+var player, obstacle, banana, back, ground;
 
 function preload(){
+  bananaImage = loadImage ("banana.png");
   
-  bananaImage = loadImage("banana.png")
-  obstacleImage = loadImage("stone.png.png")
+  obstacleImage = loadImage ("stone.png");
   
-  playerImage = loadAnimation                  ("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png")
+  backImage = loadImage ("jungle.png");
   
-  backImage = loadImage("jungle.png")
+  player_running = loadAnimation ("Monkey_01.png", "Monkey_02.png", "Monkey_03.png", "Monkey_04.png", "Monkey_05.png", "Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png")
   
 }
-
 function setup() {
   createCanvas(400, 400);
+  back = createSprite (200,200);
+  back.addImage (backImage);
   
-  banana = createSprite("200,200")
-  banana.addAnimation("banana1",bananaImage)
+  player = createSprite(50,335);
+  player.addAnimation("monkey", player_running);
   
-  obstacle = createSprite("200,100")
-  obstacle.addAnimation("obstacle1",obstacleImage)
-
+  obstacle = createSprite();
+  obstacle.addImage(obstacleImage);
   
-  player = createSprite("150,150")
-  player.addAnimation("player1",playerImage)
+  banana = createSprite();
+  banana.addImage(bananaImage);
   
-  back = createSprite("100,200")
-  back.addAnimation("back1",backImage)  
+  ground = createSprite(380,200,400,10);
+  ground.visible = false;
   
   score = 0;
-  
 }
 
 function draw() {
   background(220);
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  text("Score: "+ score, 300,50);
   drawSprites();
 }
